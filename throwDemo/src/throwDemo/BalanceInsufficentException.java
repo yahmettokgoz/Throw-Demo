f4f326b263ebc99514378dd4d72c7bdf8353e3ae
Yusuf Ahmet Tokgoz <ahmetyusuftokgoz@gmail.com>
2023-10-26 21:08:10 +0300

    Throw Demo

---
+++ throwDemo/src/throwDemo/BalanceInsufficentException.java
@@ -0,0 +1,17 @@
+package throwDemo;
+
+public class BalanceInsufficentException extends Exception {
+
+	String message;
+	
+	public BalanceInsufficentException(String message) {
+		this.message=message;
+	}
+	
+@Override
+public String getMessage() {
+	return this.message;
+}
+	
+	
+}
---
+++ throwDemo/src/throwDemo/Main.java
@@ -0,0 +1,28 @@
+package throwDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		AccountManager accountManager = new AccountManager();
+		System.out.println("Hesap = " + accountManager.getBalance());
+		accountManager.deposit(100);
+		System.out.println("Hesap = " + accountManager.getBalance());
+		try {
+			accountManager.withdraw(90);
+		} catch (Exception exception) {
+		System.out.println(exception.getMessage());
+		}
+		System.out.println("Hesap = " + accountManager.getBalance());
+		try {
+			accountManager.withdraw(20);
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
+		}
+		System.out.println("Hesap = " + accountManager.getBalance());
+
+		
+		
+		
+	}
+}
